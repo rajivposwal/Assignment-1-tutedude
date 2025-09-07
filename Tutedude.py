cc5543f848ed4572f2fb4a60d6949e3d9a09788a
rajivposwal <142899456+rajivposwal@users.noreply.github.com>
2025-09-07 17:11:41 +0530

    Add files via upload
    assignment uploaded

---
+++ Tutedude.py
@@ -0,0 +1,26 @@
+#calculator
+'''a = int(input("a: "))
+b = int(input("b: "))
+
+print("chose a opration +=1, -=2, *=3, /=4")
+c = int(input())
+print("ans")
+if c==1:
+    print(a+b)
+elif c==2:
+    print(a-b)
+elif c==3:
+    print(a*b)
+elif c==4:
+    print(a/b)
+else:
+    print("not defined")'''
+#personlized greeting
+
+print("Enter your first name: ")
+print("Enter your last name: ")
+firstname = input()
+print("Enter your last name: ")
+lastname = input()
+Names =   firstname +" " + lastname
+print(" Hello! " + Names + " Welcome to the python program")
